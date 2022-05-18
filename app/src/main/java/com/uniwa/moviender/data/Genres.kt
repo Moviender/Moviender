acfeabc80765683e4f1d6cc32c19118b97f7bd39
DTselikis <dimitriostselikis@gmail.com>
2022-05-18 16:38:48 +0300

    Implemented functionality to let you choose the list of genres for user session

--- app/src/main/java/com/uniwa/moviender/data/Genres.kt
+++ app/src/main/java/com/uniwa/moviender/data/Genres.kt
@@ -23,3 +23,25 @@ val genres: HashMap<Int, Int> = hashMapOf(
     10752 to R.string.genre_war,
     37 to R.string.genre_western
 )
+
+val nameToId: HashMap<Int, Int> = hashMapOf(
+    R.string.genre_action to 28,
+    R.string.genre_adventure to 12,
+    R.string.genre_animations to 16,
+    R.string.genre_comedy to 35,
+    R.string.genre_crime to 80,
+    R.string.genre_documentary to 99,
+    R.string.genre_drama to 18,
+    R.string.genre_family to 10751,
+    R.string.genre_fantasy to 14,
+    R.string.genre_history to 36,
+    R.string.genre_horror to 27,
+    R.string.genre_music to 10402,
+    R.string.genre_mystery to 9648,
+    R.string.genre_romance to 10749,
+    R.string.genre_science_fiction to 878,
+    R.string.genre_tv_movie to 10770,
+    R.string.genre_thriller to 53,
+    R.string.genre_war to 10752,
+    R.string.genre_western to 37
+)
