1cf60f561e1cab3cb969ca4d8ea6539492a6668e
DTselikis <dimitriostselikis@gmail.com>
2022-05-22 18:00:16 +0300

    Fixed a bug where movies where not retrieved with the right order from Room

--- app/src/main/java/com/uniwa/moviender/database/session/SessionDao.kt
+++ app/src/main/java/com/uniwa/moviender/database/session/SessionDao.kt
@@ -17,10 +17,16 @@ interface SessionDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertSessionMovieCrossRef(crossRefs: List<SessionMovieCrossRef>)
 
-    @Query("SELECT * FROM Movies " +
-            "JOIN SessionMovieCrossRef ON Movies.movielens_id = SessionMovieCrossRef.movielens_id " +
-            "JOIN Sessions ON SessionMovieCrossRef.session_id = Sessions.session_id " +
-            "WHERE Sessions.session_id = :sessionId")
+    // Used ORDER BY because movies was show sorted by primary key,
+    // which is string. This also solved flickering problem where
+    // one card showed above other because of re-sorting after
+    // new data arrived
+    @Query(
+        "SELECT * FROM Movies " +
+                "JOIN SessionMovieCrossRef ON Movies.movielens_id = SessionMovieCrossRef.movielens_id " +
+                "JOIN Sessions ON SessionMovieCrossRef.session_id = Sessions.session_id " +
+                "WHERE Sessions.session_id = :sessionId ORDER BY order_id ASC"
+    )
     fun getSessionMovies(sessionId: String): PagingSource<Int, Movie>
 
     // By deleting the session record the deletion will be cascaded
