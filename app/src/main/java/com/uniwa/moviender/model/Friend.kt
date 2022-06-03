43a245b74508182b0eef690f42690c5a839b1a67
DTselikis <dimitriostselikis@gmail.com>
2022-06-03 17:15:13 +0300

    Changed profile_pic_url to no have null values

--- app/src/main/java/com/uniwa/moviender/model/Friend.kt
+++ app/src/main/java/com/uniwa/moviender/model/Friend.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.Json
 data class Friend(
     @Json(name = "uid") val uid: String,
     @Json(name = "username") val username: String,
-    @Json(name = "profile_pic_url") val profilePicUrl: String?,
+    @Json(name = "profile_pic_url") val profilePicUrl: String,
     @Json(name = "state") val state: Int
 )
 
