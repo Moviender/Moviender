daf556d2f5caf9973b626446d0e619a37da36dab
DTselikis <dimitriostselikis@gmail.com>
2022-06-05 11:36:38 +0300

    User profile pic cannot be null

--- app/src/main/java/com/uniwa/moviender/model/User.kt
+++ app/src/main/java/com/uniwa/moviender/model/User.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class User(
     @Json(name = "uid") val uid: String,
     @Json(name = "username") val userName: String,
-    @Json(name = "profile_pic_url") val profilePicUrl: String?
+    @Json(name = "profile_pic_url") val profilePicUrl: String
 )
