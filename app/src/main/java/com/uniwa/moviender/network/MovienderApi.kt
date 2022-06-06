700e3451e06d603778c4da308b5d8f3bd9604e3d
DTselikis <dimitriostselikis@gmail.com>
2022-06-06 11:25:00 +0300

    Created Api clients

--- app/src/main/java/com/uniwa/moviender/network/MovienderApi.kt
+++ app/src/main/java/com/uniwa/moviender/network/MovienderApi.kt
@@ -7,6 +7,10 @@ import com.uniwa.moviender.network.apiservice.FriendService
 import com.uniwa.moviender.network.apiservice.MovieService
 import com.uniwa.moviender.network.apiservice.SessionService
 import com.uniwa.moviender.network.apiservice.UserService
+import com.uniwa.moviender.network.client.FriendClient
+import com.uniwa.moviender.network.client.MovieClient
+import com.uniwa.moviender.network.client.SessionClient
+import com.uniwa.moviender.network.client.UserClient
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -28,9 +32,14 @@ private val retrofit = Retrofit.Builder()
     .client(client)
     .build()
 
+private val userService: UserService by lazy { retrofit.create(UserService::class.java) }
+private val friendService: FriendService by lazy { retrofit.create(FriendService::class.java) }
+private val movieService: MovieService by lazy { retrofit.create(MovieService::class.java) }
+private val sessionService: SessionService by lazy { retrofit.create(SessionService::class.java) }
+
 object MovienderApi {
-    val userService: UserService by lazy { retrofit.create(UserService::class.java) }
-    val friendService: FriendService by lazy { retrofit.create(FriendService::class.java) }
-    val movieService: MovieService by lazy { retrofit.create(MovieService::class.java) }
-    val sessionService: SessionService by lazy { retrofit.create(SessionService::class.java) }
+    val userClient = UserClient(userService)
+    val friendClient = FriendClient(friendService)
+    val movieClient = MovieClient(movieService)
+    val sessionClient = SessionClient(sessionService)
 }
