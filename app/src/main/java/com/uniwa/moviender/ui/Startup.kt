33d9fae18726dc7d75387c498fc40bba609b4715
DTselikis <dimitriostselikis@gmail.com>
2022-06-07 12:24:53 +0300

    Added bottom navigation view

--- app/src/main/java/com/uniwa/moviender/ui/Startup.kt
+++ app/src/main/java/com/uniwa/moviender/ui/Startup.kt
@@ -1,12 +1,35 @@
 package com.uniwa.moviender.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.uniwa.moviender.R
+import com.uniwa.moviender.databinding.ActivityStartupBinding
 
 class Startup : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_startup)
+
+        binding = ActivityStartupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navView: BottomNavigationView = binding.navView
+
+        // Workaround for the FragmentContainerView bug
+        // https://issuetracker.google.com/issues/142847973
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.startup_nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navView.setupWithNavController(navController)
     }
 }
